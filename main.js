b9bed58dabfedc2349c89e4a807b64723ab15183
MyEvenWay <umarov.doniyor.2002@gmail.com>
2020-07-27 02:31:48 +0500

    Snake game completly coded

--- main.js
+++ main.js
@@ -1,82 +1,145 @@
-///////////// taking CANVAS ////////////////////////
-let can = document.getElementById("snake");
-let c = can.getContext("2d");
+//getting canvas element
+let cvs = document.getElementById("snake");
+let c = cvs.getContext("2d");
 
-////////////////// creating Snake body ///////////////
-// CONST
+//const
 const box = 32;
-let x = Math.floor(Math.random() * 19) * box;
-let y = Math.floor(Math.random() * 19) * box;
 
-let a = Math.floor(Math.random() * 19) * box;
-let b = Math.floor(Math.random() * 19) * box;
+//Load images
+const ground = new Image();
+ground.src = "./img/ground.png";
 
-let snakeArrow = [[x, y]];
+const foodImg = new Image();
+foodImg.src = "./img/food.png";
 
-function toUp() {
-  if( snakeArrow[0][1] >= box ){
-    snakeArrow[0][1] -= box;
+// load audio files
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// creating our hero( Snake )
+
+let snake = [];
+
+snake[0] = {
+  x: 9 * box,
+  y: 10 * box,
+};
+
+// creating our hero's food
+
+let food = {
+  x: Math.floor(Math.random() * 17 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 3) * box,
+};
+
+// Score
+let score = 0;
+
+//running
+let d;
+
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+  if (event.keyCode === 37 && d !== "RIGHT") {
+    d = "LEFT";
+    left.play();
+  } else if (event.keyCode === 38 && d !== "DOWN") {
+    d = "UP";
+    up.play();
+  } else if (event.keyCode === 39 && d !== "LEFT") {
+    d = "RIGHT";
+    right.play();
+  } else if (event.keyCode === 40 && d !== "UP") {
+    d = "DOWN";
+    down.play();
   }
 }
 
-function toDown() {
-  if( snakeArrow[0][1] >= box ){
-    snakeArrow[0][1] += box;
+// chech collision
+function collision(head, arr) {
+  for (let j = 0; j < arr.length; j++) {
+    if (head.x === arr[j].x && head.y === arr[j].y) {
+      return true;
+    }
   }
+
+  return false;
 }
 
-function toRight() {
-  if( snakeArrow[0][0] >= box ){
-    snakeArrow[0][0] += box;
+//drawing
+function draw() {
+  c.drawImage(ground, 0, 0);
+
+  for (let i = 0; i < snake.length; i++) {
+    c.fillStyle = i == 0 ? "green" : "white";
+    c.fillRect(snake[i].x, snake[i].y, box, box);
+
+    c.strokeStyle = "red";
+    c.strokeRect(snake[i].x, snake[i].y, box, box);
   }
+
+  c.drawImage(foodImg, food.x, food.y);
+
+  // previous positions of the hero's head
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  //choose the direction
+  if (d === "LEFT") snakeX -= box;
+  if (d === "RIGHT") snakeX += box;
+  if (d === "UP") snakeY -= box;
+  if (d === "DOWN") snakeY += box;
+
+  //snake eats food
+  if (snakeX === food.x && snakeY === food.y) {
+    score++;
+    eat.play()
+    food = {
+      x: Math.floor(Math.random() * 17 + 1) * box,
+      y: Math.floor(Math.random() * 15 + 3) * box,
+    };
+  } else {
+    //remove the tail
+    snake.pop();
+  }
+
+  //adding new Head
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  // game over
+  if (
+    snakeX < box ||
+    snakeX > 17 * box ||
+    snakeY < 3 * box ||
+    snakeY > 17 * box ||
+    collision(newHead, snake)
+  ) {
+    clearInterval(game);
+    dead.play()
+  }
+
+  snake.unshift(newHead);
+
+  c.fillStyle = "#fff";
+  c.font = "45px roboto";
+  c.fillText(score, 2 * box, 1.6 * box);
 }
 
-function toLeft() {
-  if( snakeArrow[0][0] >= box ){
-    snakeArrow[0][0] -= box;
-  }
-}
-
-function creatingSnake() {
-
-  c.clearRect( 0, 0, 608, 608 )
-
-  c.beginPath();
-  c.rect(snakeArrow[0][0], snakeArrow[0][1], box, box);
-  c.fillStyle = "blue";
-  c.fill();
-  c.closePath();
-
-  c.beginPath();
-  c.rect(a, b, box, box);
-  c.fillStyle = "red";
-  c.fill();
-  c.closePath();
-}
-
-setInterval(() => { 
-  creatingSnake()
-}, 100);
-
-////////////////////////////////////////////////////////
-///////////// Action of the Snake //////////////////////
-
-window.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "ArrowUp":
-      toUp();
-      break;
-
-    case "ArrowDown":
-      toDown();
-      break;
-
-    case "ArrowRight":
-      toRight();
-      break;
-
-    case "ArrowLeft":
-      toLeft();
-      break;
-  }
-});
+//call draw() every 100ms
+let game = setInterval(draw, 100);
